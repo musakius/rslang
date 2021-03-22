2a38022349ef1995fa7e2cb545c971eb94cb6f90
Iragemini <iragemini@gmail.com>
2021-03-22 22:57:21 +0300

    feat: add structure

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <NavLink to='/' className="navbar-brand">
+                    RSLang
+                </NavLink>
+                <div className="collapse navbar-collapse" id="navbarColor03">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to='/textbook' className="nav-link">
+                                Учебник
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/games' className="nav-link">
+                                Мини-игры
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to='/stat' className="nav-link">
+                                Статистика
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
