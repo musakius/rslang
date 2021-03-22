1f5e3e5559b12d055f5497cbeb92c679bf550828
Iragemini <iragemini@gmail.com>
2021-03-22 23:26:17 +0300

    feat: add main page structure

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Features from './components/Features/Features';
+import Video from './components/Video/Video';
+import Team from './components/Team/Team';
 
 import classes from './Main.module.scss';
 
 const Main = () => {
-  return <main className={classes['container-main']}>Main</main>;
+  return <main className={classes['container-main']}>
+    <Features/>
+    <Video/>
+    <Team/>
+  </main>;
 };
 
 export default Main;
