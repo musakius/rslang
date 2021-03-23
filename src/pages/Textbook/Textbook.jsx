a9c3bbad3dd87127163104be23d5cc8a46b5b26c
Iragemini <iragemini@gmail.com>
2021-03-23 23:44:41 +0300

    feat: add textbook components

--- src/pages/Textbook/Textbook.jsx
+++ src/pages/Textbook/Textbook.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Dictionary from './Components/Dictionary/Dictionary';
+import Links from './Components/Links/Links';
+import Pagination from './Components/Pagination/Pagination';
+import Sections from './Components/Sections/Sections';
+import Settings from './Components/Settings/Settings';
 
 const Textbook = () => {
-  return <div>TEXTBOOK</div>;
-};
+    return (
+        <div>
+            <Sections/>
+            <Dictionary/>
+            <Settings/>
+            <Links/>
+            <Pagination/>
+        </div>
+    );
+}
 
 export default Textbook;
