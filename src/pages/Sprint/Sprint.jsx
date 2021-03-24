0e775d4e4da92f0c0a20ade615674297ad6105a4
Petr <musakius.work@gmail.com>
2021-03-24 12:26:27 +0300

    feat: add Game

--- src/pages/Sprint/Sprint.jsx
+++ src/pages/Sprint/Sprint.jsx
@@ -1,7 +1,48 @@
 import React, {useState} from 'react';
+import Game from './components/Game';
 
 function Sprint() {
-  return <h1>Sprint</h1>;
+  const [initGame, setInitGame] = useState(false);
+  const [startGame, setStartGame] = useState(false);
+  const [overGame, setOverGame] = useState(false);
+  const [soundStatus, setSoundStatus] = useState(false);
+  const [learnedWords, setLearnedWords] = useState(false);
+  const [score, setScore] = useState(0);
+  const [words, setWords] = useState({});
+  const [results, setResults] = useState([]);
+  const [level, setLevel] = useState(0);
+  const [marksCombo, setMarksCombo] = useState(0);
+  const [marks, setMarks] = useState(['empty', 'empty', 'empty']);
+  const [targets, setTargets] = useState(['empty', 'empty', 'empty']);
+  const [rate, setRate] = useState(1);
+
+  return (
+    <>
+      <h1>Sprint</h1>
+      <Game
+        setOverGame={setOverGame}
+        setResults={setResults}
+        setMarks={setMarks}
+        setWords={setWords}
+        setStartGame={setStartGame}
+        setTargets={setTargets}
+        setRate={setRate}
+        setScore={setScore}
+        setSoundStatus={setSoundStatus}
+        setMarksCombo={setMarksCombo}
+        words={words}
+        startGame={startGame}
+        level={level}
+        score={score}
+        marks={marks}
+        targets={targets}
+        rate={rate}
+        soundStatus={soundStatus}
+        learnedWords={learnedWords}
+        marksCombo={marksCombo}
+      ></Game>
+    </>
+  );
 }
 
 export default Sprint;
---
+++ src/pages/Sprint/components/Game/Game.jsx
@@ -0,0 +1,255 @@
+import React, {useState, useCallback, useMemo} from 'react';
+import Exit from '../../../../common/components/Modals/Exit';
+import Rules from '../../../../common/components/Modals/Rules';
+
+import Timer from '../Timer/Timer';
+import useAPI from '../../../../common/utils';
+import {userIdSelector} from '../../../../auth/redux/selectors';
+
+import classes from './Game.module.scss';
+import {incCombo} from '../../redux'; //TODO
+
+const audioPath = 'https://raw.githubusercontent.com/' + 'alekchaik/rslang-data/master/';
+
+const fetchOptions = {
+  method: 'GET'
+};
+
+let targetsCombo = 0;
+let activeMarks = ['empty', 'empty', 'empty'];
+let activeTargets = ['empty', 'empty', 'empty'];
+
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+function Game({
+  setOverGame,
+  setResults,
+  setMarks,
+  setWords,
+  setStartGame,
+  setTargets,
+  setRate,
+  setScore,
+  setSoundStatus,
+  setMarksCombo,
+  words,
+  startGame,
+  level,
+  score,
+  marks,
+  targets,
+  rate,
+  soundStatus,
+  learnedWords,
+  marksCombo
+}) {
+  const userID = useSelector(userIdSelector);
+
+  const [count, setCount] = useState(0);
+
+  const userWordsURL = useMemo(
+    () =>
+      learnedWords
+        ? `users/${userID}/aggregatedWords?wordsPerPage=150&filter={"userWord.optional.deleted":false}`
+        : `words?page=0&group=${level}
+      &wordsPerExampleSentenceLTE=1000&wordsPerPage=150`,
+    [level, userID, learnedWords]
+  );
+
+  const action = useCallback(
+    (data) => {
+      const path = learnedWords ? data[0].paginatedResults : data;
+      path.forEach((el) => {
+        el.falsyTranslate = el.wordTranslate;
+      });
+      path.forEach((el) => {
+        el.falsyTranslate = getRandomInt(2)
+          ? path[getRandomInt(path.length - 1)].falsyTranslate
+          : el.falsyTranslate;
+        el.correctFlag = el.falsyTranslate === el.wordTranslate;
+      });
+      setWords(path);
+    },
+    [learnedWords]
+  );
+
+  useAPI(userWordsURL, fetchOptions, action);
+
+  const changeScore = useCallback(
+    (bool) => {
+      if (bool) setScore(score + rate * 10);
+    },
+    [rate, score]
+  );
+
+  const changeTargets = useCallback(
+    (bool) => {
+      activeTargets = targets;
+      if (bool && targetsCombo < 3) {
+        activeTargets[targetsCombo] = 'hit';
+        targetsCombo += 1;
+        setRate(rate + 1);
+      }
+      if (!bool) {
+        targetsCombo = 0;
+        activeTargets = ['empty', 'empty', 'empty'];
+        setRate(1);
+      }
+      setTargets([...activeTargets]);
+    },
+    [targets, rate]
+  );
+
+  const changeMark = useCallback(
+    (bool) => {
+      activeMarks = [...marks];
+      if (bool && marksCombo < 3) {
+        activeMarks[marksCombo] = 'hit';
+        incCombo();
+      } else if (bool && marksCombo >= 3) {
+        activeMarks = ['empty', 'empty', 'empty'];
+        setMarksCombo(0);
+        changeTargets(true);
+      } else {
+        setMarksCombo(0);
+        activeMarks = ['empty', 'empty', 'empty'];
+        changeTargets(false);
+      }
+      setMarks([...activeMarks]);
+    },
+    [marks, changeTargets, marksCombo]
+  );
+
+  const audioRight = useCallback(() => {
+    const RightAudio = new Audio('/assets/audio/right.mp3');
+    RightAudio.play();
+  }, []);
+
+  const audioWrong = useCallback(() => {
+    const WrongAudio = new Audio('/assets/audio/wrong.mp3');
+    WrongAudio.play();
+  }, []);
+
+  const pronounce = useCallback(
+    (n) => {
+      const pronounce = new Audio(`${audioPath}${words[n].audio}`);
+      pronounce.play();
+    },
+    [words]
+  );
+
+  const onAnswer = useCallback(
+    (word, answer) => {
+      word.correctAnswer = word.correctFlag === answer;
+      if (soundStatus) {
+        word.correctAnswer ? audioRight() : audioWrong();
+      }
+      changeMark(word.correctAnswer);
+      changeScore(word.correctAnswer);
+      setResults([word]); //2
+    },
+    [dispatch, changeMark, changeScore, soundStatus, audioRight, audioWrong]
+  );
+
+  /* const onExit = useCallback(() => {
+    dispatch(setDefault());
+  }, [dispatch]); */
+
+  if (startGame) {
+    return (
+      <div className="Main">
+        <Rules rules="Если указан верный перевод слова - нажимайте 'Верно'" />
+        <Exit /* onExit={onExit} */ noWhite={false} />
+        <div className="UpperContainer">
+          <div className="TaimerContainer">
+            <Timer initialTime={60} timeOutHandler={() => setOverGame(true)} />
+          </div>
+
+          <div className="ScoreContainer">
+            <p className="Score">{score}</p>
+          </div>
+          <div className="Toolbar">
+            <label className="Notification_label">
+              <input
+                onChange={() => {
+                  setSoundStatus(!soundStatus);
+                }}
+                className="Notification_input"
+                type="checkbox"
+                value="1"
+                name="k"
+              />
+              <span />
+            </label>
+          </div>
+        </div>
+        <div className="BlockWordContainer">
+          <div className="BlockWord">
+            <div className="Marks">
+              {marks.map((type, index) => (
+                <img key={index} src={`/assets/images/sprint/${type}_mark.svg`} alt="mark" />
+              ))}
+            </div>
+
+            <div className="Targets">
+              <img src="/assets/images/sprint/hit_target.svg" alt="hint" />
+              {targets.map((type, index) => (
+                <img key={index} src={`/assets/images/sprint/${type}_target.svg`} />
+              ))}
+            </div>
+
+            <div className="Words">
+              <p className="EnWord">{words[count].word}</p>
+              <p className="RuWord">{words[count].falsyTranslate}</p>
+            </div>
+
+            <div className="Buttons">
+              <button
+                className="Btn False"
+                onClick={() => {
+                  setCount(count + 1);
+                  onAnswer(words[count], false);
+                  if (count === words.length - 1) setOverGame(true);
+                }}
+              >
+                Не верно
+              </button>
+              <button
+                className="Btn True"
+                onClick={() => {
+                  setCount(count + 1);
+                  onAnswer(words[count], true);
+                  if (count === words.length - 1) setOverGame(true);
+                }}
+              >
+                Верно
+              </button>
+            </div>
+
+            <div className="Arrows">
+              <img className="Left" src="/assets/images/sprint/left_arrow.svg" alt="arrow left" />
+              <img
+                className="Right"
+                src="/assets/images/sprint/right_arrow.svg"
+                alt="arrow right"
+              />
+            </div>
+
+            <button
+              className="pronounce"
+              onClick={() => {
+                pronounce(count);
+              }}
+              type="button"
+            >
+              <img className="pronounce_img" src="/assets/images/sprint/sound.svg" />
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return <Timer initialTime={5} timeOutHandler={() => setStartGame(true)} />;
+}
+
+export default Game;
