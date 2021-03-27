aeeeba3fbb9e3bea1519afcc563f135825f26d9d
Iragemini <iragemini@gmail.com>
2021-03-27 19:41:50 +0300

    feat: add Carousel

--- src/pages/Textbook/Components/SectionContent/Page.jsx
+++ src/pages/Textbook/Components/SectionContent/Page.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import WordCard from "./WordCard";
 import Pagination from "../Pagination/";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Page = ({wordsSet}) => {
   return (
-  <div className='container'>
+  <div className=' container carousel-wrapper'>
+    <Carousel>
       {
           wordsSet.map((word) => <WordCard key={word.id} wordObj={word} />)
       }
-          <Pagination />
+      </Carousel>
   </div>);
 };
 
