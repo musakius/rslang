14ba0c9a2c271791c62c17dd37d92f972066f51f
Petr <musakius.work@gmail.com>
2021-03-31 14:57:02 +0300

    fead: add route game-member && game-sprint

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router";
-import Games from "../pages/Games";
-import Login from "../pages/Login";
-import Main from "../pages/Main";
-import Page404 from "../pages/Page404";
-import Register from "../pages/Register";
-import Sprint from "../pages/Sprint";
-import Stat from "../pages/Statistics";
-import Textbook from "../pages/Textbook";
-import Dictionary from "../pages/Textbook/Components/Dictionary/Dictionary";
+import React from 'react';
+import {Switch, Route, Redirect} from 'react-router';
+import Games from '../pages/Games';
+import Login from '../pages/Login';
+import Main from '../pages/Main';
+import Page404 from '../pages/Page404';
+import Register from '../pages/Register';
+import GameSprint from '../pages/GameSprint';
+import GameMemory from '../pages/GameMemory';
+import Stat from '../pages/Statistics';
+import Textbook from '../pages/Textbook';
+import Dictionary from '../pages/Textbook/Components/Dictionary/Dictionary';
 
 export const useRoutes = (isAuthenticated) => {
   if (isAuthenticated) {
     return (
       <Switch>
-        <Route path='/' exact render={() => <Main />} />
-        <Route path='/textbook' render={() => <Textbook />} />
-        <Route path='/dictionary' render={() => <Dictionary />} />
-        <Route path='/games' exact render={() => <Games />} />
-        <Route path='/games/sprint' render={() => <Sprint />} />
-        <Route path='/stat' render={() => <Stat />} />
-        <Redirect to='/' />
+        <Route path="/" exact render={() => <Main />} />
+        <Route path="/textbook" render={() => <Textbook />} />
+        <Route path="/dictionary" render={() => <Dictionary />} />
+        <Route path="/games" exact render={() => <Games />} />
+        <Route path="/games/sprint" render={() => <GameSprint />} />
+        <Route path="/games/memory" render={() => <GameMemory />} />
+        <Route path="/stat" render={() => <Stat />} />
+        <Redirect to="/" />
         <Route render={() => <Page404 />} />
       </Switch>
     );
   }
   return (
     <Switch>
-      <Route path='/login' exact render={() => <Login />} />
-      <Route path='/register' exact render={() => <Register />} />
-      <Redirect to='/login' />
+      <Route path="/login" exact render={() => <Login />} />
+      <Route path="/register" exact render={() => <Register />} />
+      <Redirect to="/login" />
     </Switch>
   );
 };
