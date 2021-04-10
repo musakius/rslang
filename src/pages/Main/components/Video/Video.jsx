4c9c71b0707fff56cd4c2792563b193358538b2c
Danila-cmd <dvfeduleev@gmail.com>
2021-04-10 19:35:13 +0500

    fix: some error on Register component

--- src/pages/Main/components/Video/Video.jsx
+++ src/pages/Main/components/Video/Video.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
+import './Video.css'
 
 const Video = () => {
     return (
-        <div>
-            VIDEO
+        <div className='app'>
+            <ReactPlayer
+                controls
+                url='https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=177s'
+                height="360px"
+                width="640px"
+            />
         </div>
     );
 }
