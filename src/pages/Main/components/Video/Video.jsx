952e496e8a317d8585b286b18093f1da0bbfb32c
Danila-cmd <dvfeduleev@gmail.com>
2021-04-10 19:46:52 +0500

    fix: error in Video component

--- src/pages/Main/components/Video/Video.jsx
+++ src/pages/Main/components/Video/Video.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactPlayer from 'react-player';
-=======
 import ReactPlayer from 'react-player'
->>>>>>> 82238991c483136a4f7e7a7598a34becfa7e4100
 import './Video.css'
 
 const Video = () => {
@@ -11,11 +7,7 @@ const Video = () => {
         <div className='app'>
             <ReactPlayer
                 controls
-<<<<<<< HEAD
-                url='https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=177s'
-=======
                 url='https://www.youtube.com/watch?v=w7ejDZ8SWv8'
->>>>>>> 82238991c483136a4f7e7a7598a34becfa7e4100
                 height="360px"
                 width="640px"
             />
