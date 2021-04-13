daf2f67154abcb45811ee1f73ffe6cf449eae193
Irina Karavaichyk <iragemini@gmail.com>
2021-04-13 08:48:42 +0300

    merge: merge develop

--- src/components/Header/Select.jsx
+++ src/components/Header/Select.jsx
@@ -1,22 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { items } from "../../pages/Textbook/config";
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { items } from '../../pages/Textbook/config';
+import classes from './Header.module.scss';
 
 const Select = () => {
-    console.log('items', items);
+  const [title, setTitle] = useState('Учебник');
+
+  useEffect(() => {
+    showMenu();
+  }, [title]);
+  const showMenu = () => {
+    document.getElementById('btnSelect').classList.toggle('show')
+    console.log('tt');
+  }
   return (
-    <label>
-      Учебник
-      <select className='nav-item dropdown' value={""} onChange={() => {}}>
-        {items.map((item) => (
-          <option key={item.group} value={item.group}>
-            <Link key={item.group} to={`/textbook/group/${item.group}`}>
+    <div
+      className="btn-group"
+      role="group"
+      aria-label="Button group with nested dropdown"
+    >
+      <div type="button" className="btn btn-light nav-item">
+        <NavLink to="/textbook" className="nav-link p-0">
+          <div  className='text-secondary'>{title}</div>
+        </NavLink>
+      </div>
+      <div className="btn-group" role="group">
+        <button
+          id="btnMenu"
+          type="button"
+          className="btn btn-light dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          onClick={() => showMenu()}
+        ></button>
+        <div id='btnSelect' className="dropdown-menu" aria-labelledby="btnMenu">
+          {items.map((item) => (
+            <NavLink key={item.group} to={`/textbook/group/${item.group}`} className="dropdown-item" onClick={() => setTitle(item.item)}>
               {item.item}
-            </Link>
-          </option>
-        ))}
-      </select>
-    </label>
+            </NavLink>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
