50b1a6277a9020b6dbcf19277f2c50829b5d8639
Irina Karavaichyk <iragemini@gmail.com>
2021-04-16 08:58:14 +0300

    fix: fix routes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import {Provider} from 'react-redux';
 import store from './redux/store';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
